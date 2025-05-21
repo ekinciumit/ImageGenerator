d215cf208fa62d2d218a37b7a212e7a72f33eb98
ekinciumit <128986680+ekinciumit@users.noreply.github.com>
2025-05-21 19:29:49 +0300

    Update util.kt

--- app/src/main/java/com/example/imagegenerator/util.kt
+++ app/src/main/java/com/example/imagegenerator/util.kt
@@ -1,5 +1,5 @@
 package com.example.imagegenerator
 
 //Sign up Wiro dashboard and create project
-val YOUR_API_KEY = "netnt1wsvf5228jbzqsf9fd3jvitore2";
-val YOUR_API_SECRET = "3d307e9e37efbe4a38dffdd665f5ef2580fc686c9a9f7becabbf7cb9b24efa5a";
+val YOUR_API_KEY = "";
+val YOUR_API_SECRET = "";
